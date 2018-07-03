067dbe2dd42cf0fcf7c9c271f2f333b61a7be43d
liang <liang@sogou-inc.com>
2018-07-03 16:58:44 +0800

    CombinationSum and ConbinationSumII

---
+++ src/search/dfs/CombinationSum/SolutionTest.kt
@@ -0,0 +1,15 @@
+package search.dfs.CombinationSum
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SolutionTest {
+
+    @Test
+    fun combinationSum() {
+        println(Solution().combinationSum(intArrayOf(2, 3, 6, 7), 7))
+        println(Solution().combinationSum(intArrayOf(2, 3, 5), 8))
+
+    }
+}
---
+++ src/search/dfs/CombinationSumII/SolutionTest.kt
@@ -0,0 +1,14 @@
+package search.dfs.CombinationSumII
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SolutionTest {
+
+    @Test
+    fun combinationSum2() {
+        println(Solution().combinationSum2(intArrayOf(2, 5, 2, 1, 2), 5))
+        println(Solution().combinationSum2(intArrayOf(10,1,2,7,6,1,5), 8))
+    }
+}
