96fa654e9f7452c62724e0b53058dac4ea681356
liang <liang@sogou-inc.com>
2018-08-07 19:35:03 +0800

    add ReverseKGroup

---
+++ src/list/single/ReverseNodesInKGroup/SolutionTest.kt
@@ -0,0 +1,34 @@
+package list.single.ReverseNodesInKGroup
+
+import list.single.toArrayList
+import list.single.toLinkedList
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.util.*
+
+class SolutionTest {
+
+    @Test
+    fun reverseKGroup() {
+        helper(intArrayOf(1, 2, 3, 4, 5), 2, intArrayOf(2, 1, 4, 3, 5))
+        helper(intArrayOf(1, 2, 3, 4, 5), 3, intArrayOf(3, 2, 1, 4, 5))
+    }
+
+    @Test
+    fun revert() {
+        val head = intArrayOf(1,2,3,4,5).toLinkedList()
+        var tail = head!!.next!!.next
+        val result = Solution().revert(head, tail)
+        val resultIntArray = result[0].toArrayList().toIntArray()
+        println(Arrays.toString(resultIntArray))
+    }
+
+    fun helper(intArray: IntArray, k: Int, expeactedIntArray: IntArray) {
+        val result = Solution().reverseKGroup(intArray.toLinkedList(), k).toArrayList().toIntArray()
+        assertArrayEquals(
+                expeactedIntArray,
+                result
+        )
+    }
+}
