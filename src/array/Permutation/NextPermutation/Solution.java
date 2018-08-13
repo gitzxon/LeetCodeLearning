b5da9849d11e361d7c0d9a41e91f4aa10595fc8f
liang <liang@sogou-inc.com>
2018-08-13 15:56:02 +0800

    （1）add SubSetII （2）NextPermutation 复习

--- src/array/Permutation/NextPermutation/Solution.java
+++ src/array/Permutation/NextPermutation/Solution.java
@@ -1,8 +1,18 @@
 package array.Permutation.NextPermutation;
 
+/**
+ * 总结一下流程：
+ * 1. 找到最长递减 suffix。
+ * 2. 找 pivot
+ * 3. suffix 中找到比 pivot 大的最小的数 X
+ * 4. swap pivot and X
+ * 5. reverse new suffix
+ */
 class Solution {
     public void nextPermutation(int[] nums) {
 
+        // 找最长递减 suffix 起点
+        // pivot = start - 1
         int pivot = -1;
         for (int i = nums.length - 1; i >= 0; i--) {
             if (i == 0 || nums[i - 1] < nums[i]) {
@@ -11,11 +21,15 @@ class Solution {
             }
         }
 
+        // pivot 无效，说明整个数组就是那个递减 suffix，
+        // ß所以，直接 reverse
         if (pivot == -1) {
             reverse(nums, 0);
             return;
         }
 
+        // suffix 中比 pivot 大的最小的数
+        // suffix 是递减的，所以从右往左找第一个即可
         int rightmostMoreThanPivot = -1;
         for (int i = nums.length - 1; i >= 0; i--) {
             if (nums[i] > nums[pivot]) {
--- src/backtracking/SubSet/SubSetII/Solution.java
+++ src/backtracking/SubSet/SubSetII/Solution.java
@@ -12,11 +12,24 @@ class Solution {
 
     private List<List<Integer>> helper(int[] nums, List<List<Integer>> resultContainer, List<Integer> currentUsedList, int startIndex) {
 
+        resultContainer.add(new ArrayList<>(currentUsedList));
 
-        for (int i = ; i < nums.length; i++) {
-
+        if (startIndex >= nums.length) {
+            return resultContainer;
         }
 
+        for (int i = startIndex; i < nums.length; i++) {
 
+            if (i > startIndex && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            currentUsedList.add(nums[i]);
+            helper(nums, resultContainer, currentUsedList, i + 1);
+            currentUsedList.remove(currentUsedList.size() - 1);
+        }
+
+        return resultContainer;
     }
+
 }
