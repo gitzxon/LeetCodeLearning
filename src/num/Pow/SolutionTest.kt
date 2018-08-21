70f247203846c0f31219019653c697dbddb0c361
liang <liang@sogou-inc.com>
2018-08-21 20:26:35 +0800

    add new todo: fast three sum

---
+++ src/num/Pow/SolutionTest.kt
@@ -0,0 +1,18 @@
+package num.Pow
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SolutionTest {
+
+    @Test
+    fun myPow() {
+
+        println(Math.pow(0.0, 0.0))
+
+        println(Solution().myPow(2.00000, -2147483648))
+
+        Double.POSITIVE_INFINITY
+    }
+}
