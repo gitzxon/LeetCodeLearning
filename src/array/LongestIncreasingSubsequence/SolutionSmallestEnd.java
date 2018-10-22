a4a8471de7e2c96fcedb21756ad1883dc067b76c
liang <liang@sogou-inc.com>
2018-10-22 14:33:10 +0800

    add longest increasing subsequence smallest end version.

--- src/array/LongestIncreasingSubsequence/SolutionSmallestEnd.java
+++ src/array/LongestIncreasingSubsequence/SolutionSmallestEnd.java
@@ -4,40 +4,67 @@ import java.util.Arrays;
 
 public class SolutionSmallestEnd {
 
+    public static class Solution {
 
-    public int lengthOfLIS(int[] nums) {
-        int[] smallestEnd = new int[nums.length];
-        smallestEnd[0] = nums[0];
-        int len = 1;
+        public int lengthOfLIS(int[] nums) {
 
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (num < smallestEnd[0]) {
-                smallestEnd[0] = num;
-            } else if (num > smallestEnd[len - 1]) {
-                smallestEnd[len] = num;
-                len += 1;
-            } else {
-                int position = binarySearch(smallestEnd, 0, )
-                smallestEnd[position] = num;
+            if (nums == null || nums.length == 0) {
+                return 0;
             }
+            if (nums.length == 1) {
+                return 1;
+            }
+
+            int[] smallestEnd = new int[nums.length];
+            smallestEnd[0] = nums[0];
+            int len = 1;
+
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i];
+                if (num < smallestEnd[0]) {
+                    smallestEnd[0] = num;
+                } else if (num == smallestEnd[0]) {
+                    // do nothing
+                } else if (num > smallestEnd[len - 1]) {
+                    smallestEnd[len] = num;
+                    len += 1;
+                } else if (num == smallestEnd[len - 1]) {
+                    // do nothing
+                } else {
+                    int position = binarySearch(smallestEnd, 0, len - 1, num);
+                    smallestEnd[position] = num;
+                }
+            }
+
+            return len;
         }
 
-        return len;
-    }
-
-    private int binarySearch(int[] nums, int start, int end, int key) {
-        int i = start;
-        int j = end;
-        while (i < j) {
-            int m = i + j / 2;
-            if (nums[m] < key) {
-                i = m + 1;
-            } else {
-                j = m - 1;
+        private int binarySearch(int[] nums, int start, int end, int key) {
+            int i = start;
+            int j = end;
+            while (i < j) {
+                int m = (i + j) / 2;
+                if (nums[m] == key) {
+                    return m;
+                } else if (nums[m] < key) {
+                    i = m + 1;
+                } else {
+                    j = m - 1;
+                }
             }
-        }
 
-        return i;
+            if (i == j) {
+                if (i == end) {
+                    return i;
+                } else {
+                    return i + 1;
+                }
+            } else {
+                return i;
+            }
+
+        }
     }
+
+
 }
