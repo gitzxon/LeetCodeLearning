cd5c82a2a73bc902005126f1872e6bd28ea91ed6
liang <mailtoliang@qq.com>
2018-11-06 22:39:09 +0800

    add PermutationString

---
+++ src/string/PermutationString/SolutionTest.kt
@@ -0,0 +1,29 @@
+package string.PermutationString
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SolutionTest {
+
+    @Test
+    fun checkInclusion() {
+        assertEquals(
+                true,
+                Solution().checkInclusion(
+                        "ab",
+                        "eidbaooo"
+                )
+        )
+    }
+
+    @Test
+    fun test1() {
+        assertEquals(
+                false,
+                Solution().checkInclusion(
+                        "ab",
+                        "eidboaoo"
+                )
+        )
+    }
+}
