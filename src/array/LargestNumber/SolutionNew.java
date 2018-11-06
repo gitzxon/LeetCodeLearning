76063193035362a8ac1e3dca7d3ff92b21c0a020
liang <liang@sogou-inc.com>
2018-11-06 20:34:26 +0800

    把 lambda 换成匿名类。用 lambda 太慢了。

--- src/array/LargestNumber/SolutionNew.java
+++ src/array/LargestNumber/SolutionNew.java
@@ -18,11 +18,20 @@ public class SolutionNew {
             integers[i] = nums[i];
         }
 
+        new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        };
 
-        Arrays.sort(integers, (o1, o2) -> {
-            String s1 = "" + o1 + o2;
-            String s2 = "" + o2 + o1;
-            return s2.compareTo(s1);
+        Arrays.sort(integers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                String s1 = "" + o1 + o2;
+                String s2 = "" + o2 + o1;
+                return s2.compareTo(s1);
+            }
         });
 
         StringBuilder stringBuilder = new StringBuilder();
