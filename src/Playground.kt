b1574a074312eeda48526c0be2df994aaf4e2b2a
liang <mailtoliang@qq.com>
2018-11-11 11:03:55 +0800

    add array/KthLargestElementInAnArray

---
+++ src/Playground.kt
@@ -0,0 +1,11 @@
+import org.junit.Test
+
+class Playground {
+
+    @Test
+    fun test() {
+        val name = "mice"
+        val age = 18
+        println("我是 %s，我今年 %d 岁".format(name, age))
+    }
+}
