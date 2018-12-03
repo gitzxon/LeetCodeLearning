67f03a1c99a89c420a39e32845b65e8de1939793
liang <mailtoliang@qq.com>
2018-12-03 11:55:35 +0800

    fix: 跑通了 wiggleSort。原来是开始的退出条件写的太粗犷了。

--- src/array/WiggleSortII/Solution.java
+++ src/array/WiggleSortII/Solution.java
@@ -5,7 +5,7 @@ class Solution {
     public void wiggleSort(int[] nums) {
 
         if (nums == null) return;
-        if (nums.length <= 2) return;
+        if (nums.length <= 1) return;
 
         int k = (nums.length + 1) / 2;
         findKthLargest(nums, k);
@@ -31,6 +31,33 @@ class Solution {
         }
     }
 
+    public void wiggleSort_good(int[] nums) {
+        int median = findKthLargest(nums, (nums.length + 1) / 2);
+        int n = nums.length;
+
+        int left = 0, i = 0, right = n - 1;
+
+        while (i <= right) {
+
+            if (nums[newIndex(i,n)] > median) {
+                swap(nums, newIndex(left++,n), newIndex(i++,n));
+            }
+            else if (nums[newIndex(i,n)] < median) {
+                swap(nums, newIndex(right--,n), newIndex(i,n));
+            }
+            else {
+                i++;
+            }
+        }
+
+
+    }
+
+    private int newIndex(int index, int n) {
+        return (1 + 2*index) % (n | 1);
+    }
+
+
     private int findKthLargest(int[] nums, int k) {
         return findKthHelper(nums, 0, nums.length - 1, k - 1);
     }
@@ -43,7 +70,7 @@ class Solution {
         int current = lo;
         int i = lo;
         while (i <= hi - 1) {
-            if (nums[i] <= pivot) {
+            if (nums[i] >= pivot) {
                 swap(nums, i, current);
                 current++;
             }
