c6d898fe0dd09170f4f72d6df8c150cb23144f5c
liang <mailtoliang@qq.com>
2018-12-18 10:13:17 +0800

    add MaximumSubarray

---
+++ src/array/MaximumSubarray/SolutionTest.kt
@@ -0,0 +1,16 @@
+package array.MaximumSubarray
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SolutionTest {
+
+    @Test
+    fun maxSubArray() {
+        assertEquals(
+                6,
+                Solution().maxSubArray(intArrayOf(-2,1,-3,4,-1,2,1,-5,4))
+        )
+
+    }
+}
