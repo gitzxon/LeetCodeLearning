744dc01f71a5879a85bcfb19a0ab4b789e0e71d3
liang <mailtoliang@qq.com>
2018-12-25 00:57:47 +0800

    three sum closet

--- src/pattern/ThreeSum/ThreeSumCloset/normal/Solution.java
+++ src/pattern/ThreeSum/ThreeSumCloset/normal/Solution.java
@@ -1,65 +1,34 @@
 package pattern.ThreeSum.ThreeSumCloset.normal;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
-package pattern.ThreeSum.ThreeSum.normal;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-
+    public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        List<List<Integer>> resultContainer = new ArrayList<>();
         int mid;
         int right;
-        for (int left = 0; left <= nums.length - 2 && nums[left] <= 0; left++) {
+        int min = 0;
+        boolean find = false;
 
-            if (left >= 1 && nums[left] == nums[left - 1]) {
-                continue;
-            }
-
+        for (int left = 0; left <= nums.length - 2; left++) {
             mid = left + 1;
             right = nums.length - 1;
 
-            int remain = 0 - nums[left];
             while (mid < right) {
-                int sum = nums[mid] + nums[right];
-                if (sum == remain) {
-                    addResult(resultContainer, nums[left], nums[mid], nums[right]);
-                    while (validIndex(nums.length, mid)
-                            && validIndex(nums.length, mid + 1)
-                            && nums[mid] == nums[mid + 1]) {
-                        mid += 1;
-                    }
-                    mid += 1;
-
-                    while (validIndex(nums.length, right)
-                            && validIndex(nums.length, right - 1)
-                            && nums[right] == nums[right - 1]) {
-                        right -= 1;
-                    }
-                    right -= 1;
-
-                } else if (sum < remain) {
-                    mid += 1;
-                } else {
-                    right -= 1;
+                int sum = nums[left] + nums[mid] + nums[right];
+                if (!find || Math.abs(target - sum) < Math.abs(target - min)) {
+                    find = true;
+                    min = sum;
                 }
+
+                if (sum > target) {
+                    right -= 1;
+                } else {
+                    mid += 1;
+                }
+
             }
         }
-        return resultContainer;
-    }
-
-    private boolean validIndex(int len, int index) {
-        return index >= 0 && index < len;
-    }
-
-    private void addResult(List<List<Integer>> list, int left, int mid, int right) {
-        List<Integer> tmp = new ArrayList<>();
-        tmp.add(left);
-        tmp.add(mid);
-        tmp.add(right);
-        list.add(tmp);
+        return min;
     }
 }
