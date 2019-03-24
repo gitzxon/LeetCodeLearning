b4426d28bb07b316f49e549198ffb0a52ffe9e25
liang <mailtoliang@qq.com>
2019-03-24 18:24:29 +0800

    add RangeSumOfBST

--- src/tree/RangeSumOfBST/Solution.java
+++ src/tree/RangeSumOfBST/Solution.java
@@ -2,50 +2,34 @@ package tree.RangeSumOfBST;
 
 import tree.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
+
+    private int mMin;
+    private int mMax;
+    private int mSum;
 
     public int rangeSumBST(TreeNode root, int leftValue, int rightValue) {
 
-        TreePathResult lResult = new TreePathResult();
-        TreePathResult rResult = new TreePathResult();
-        performDFS(lResult, new ArrayList<>(), root, leftValue);
-        performDFS(rResult, new ArrayList<>(), root, rightValue);
-
-        return 0;
+        mMin = leftValue;
+        mMax = rightValue;
+        mSum = 0;
+        performDFS(root);
+        return mSum;
     }
 
-    private void performDFS(
-            TreePathResult treePathResult,
-            List<TreeNode> curList,
-            TreeNode root,
-            int target) {
-
-        if (treePathResult.found) {
+    private void performDFS(TreeNode root) {
+        if (root == null) {
             return;
         }
 
-        curList.add(root);
-        if (root.val == target) {
-            treePathResult.found = true;
-            treePathResult.path.addAll(curList);
-        } else {
-            curList.add(root.left);
-            performDFS(treePathResult, curList, root.left, target);
-            curList.remove(curList.size() - 1);
-
-            if (!treePathResult.found) {
-                curList.add(root.right);
-                performDFS(treePathResult, curList, root.right, target);
-                curList.remove(curList.size() - 1);
-            }
+        if (root.val >= mMin && root.val <= mMax) {
+            mSum += root.val;
+            performDFS(root.left);
+            performDFS(root.right);
+        } else if (root.val < mMin) {
+            performDFS(root.right);
+        } else { //root.val > mMax
+            performDFS(root.left);
         }
-    }
-
-    private static class TreePathResult {
-        List<TreeNode> path = new ArrayList<>();
-        boolean found = false;
     }
 }
