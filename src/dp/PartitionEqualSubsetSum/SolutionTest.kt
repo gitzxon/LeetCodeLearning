8d8629780090554e6eaec27e49dc89f21bf3da99
liang <mailtoliang@qq.com>
2019-11-28 08:08:05 +0800

    ADD: add 01 pack simple version.

--- src/dp/PartitionEqualSubsetSum/SolutionTest.kt
+++ src/dp/PartitionEqualSubsetSum/SolutionTest.kt
@@ -9,10 +9,17 @@ class SolutionTest {
     @Test
     fun canPartition() {
         assertTrue(Solution().canPartition(intArrayOf(1, 5, 11, 5)))
+
+        assertTrue(
+                SolutionNew().Solution().canPartition(intArrayOf(1, 5, 11, 5))
+        )
     }
 
     @Test
     fun canPartition_1() {
         assertFalse(Solution().canPartition(intArrayOf(1,2,3,5)))
+        assertFalse(
+                SolutionNew().Solution().canPartition(intArrayOf(1,2,3,5))
+        )
     }
 }
