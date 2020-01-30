afaad69b8fbb584a49d42e544ae3d55ddcfb9d26
liang <mailtoliang@qq.com>
2020-01-30 19:21:25 +0800

    ADD: WildcardMatching.

---
+++ src/misc/WildcardMatching/SolutionTest.kt
@@ -0,0 +1,10 @@
+package misc.WildcardMatching
+
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun comparison() {
+        assert(Solution().isMatch("bbbbbbbbbbbb", "*bbbb"))
+    }
+}
