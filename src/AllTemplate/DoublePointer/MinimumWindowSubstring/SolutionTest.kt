4522cc855151cc90be8c03d43b188fe3cd2c701f
liang <mailtoliang@qq.com>
2020-04-26 11:26:19 +0800

    add two questions of DoublePointer。

---
+++ src/AllTemplate/DoublePointer/MinimumWindowSubstring/SolutionTest.kt
@@ -0,0 +1,14 @@
+package AllTemplate.DoublePointer.MinimumWindowSubstring
+
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun minWindow() {
+        val result = Solution().minWindow(
+                "ADOBECODEBANC",
+                "ABC"
+        )
+        println("result = ${result}")
+    }
+}
